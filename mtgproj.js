4cf2e1a62cc1aca4973454c93ffcc15fd6bc1e21
tran-justin <149015543+tran-justin@users.noreply.github.com>
2024-12-03 03:18:10 -0500

    added mergeSortByPrice and mergeSortByName

--- mtgproj.js
+++ mtgproj.js
@@ -1,7 +1,6 @@
 const fs = require('fs');
 const path = require('path');
 
-const collection = [];
 // Function to search for a card by name in the JSON file
 function getCardInfo(name, jsonFile = path.join(__dirname, 'default-cards.json')) {
     try {
@@ -18,11 +17,11 @@ function getCardInfo(name, jsonFile = path.join(__dirname, 'default-cards.json')
                 if (price === null) continue; // if price is null don't print info
 
                 //temp
-                collection.push({
-                    name: cards[i].name,
-                    price: cards[i]['prices']['usd'],
-                    set: cards[i]['set_name'],
-                });
+                // collection.push({
+                //     name: cards[i].name,
+                //     price: cards[i]['prices']['usd'],
+                //     set: cards[i]['set_name'],
+                // });
                 //temp
 
                 console.log(`Card found: ${cards[i].name}`);
@@ -44,14 +43,85 @@ function getCardInfo(name, jsonFile = path.join(__dirname, 'default-cards.json')
     }
 }
 
+function mergeSortByPrice(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const leftArr = arr.slice(0, mid);
+    const rightArr = arr.slice(mid);
+
+    return mergeByPrice(mergeSortByPrice(leftArr), mergeSortByPrice(rightArr));
+
+}
+
+function mergeByPrice(leftArr, rightArr){
+    const sortedArr = [];
+
+    while(leftArr.length && rightArr.length){
+        if(leftArr[0].price < rightArr[0].price){
+            sortedArr.push(leftArr.shift());
+        }
+        else {
+            sortedArr.push(rightArr.shift());
+        }
+    }
+    return [...sortedArr, ...leftArr, ...rightArr];
+}
+
+function mergeSortByName(arr) {
+    if (arr.length < 2) {
+        return arr;
+    }
+
+    const mid = Math.floor(arr.length / 2);
+    const leftArr = arr.slice(0, mid);
+    const rightArr = arr.slice(mid);
+
+    return mergeByName(mergeSortByName(leftArr), mergeSortByName(rightArr));
+
+}
+
+function mergeByName(leftArr, rightArr){
+    const sortedArr = [];
+
+    while(leftArr.length && rightArr.length){
+        if(leftArr[0].name < rightArr[0].name){
+            sortedArr.push(leftArr.shift());
+        }
+        else {
+            sortedArr.push(rightArr.shift());
+        }
+    }
+    return [...sortedArr, ...leftArr, ...rightArr];
+}
+const collection = [
+    { name: "Sol Ring", price: 1.53 },
+    { name: "Black Lotus", price: 17250.00 },
+    { name: "Doubling Season", price: 48.75 },
+    { name: "Force of Will", price: 99.99 },
+    { name: "Volrath's Stronghold", price: 21.75 }
+];
+
+// Sort by price ascending
+const sortedCollectionPrice = mergeSortByPrice(collection);
+// sortedCollectionPrice.reverse(); // switch to descending
+console.log(sortedCollectionPrice);
+
+// Sort by name ascending
+const sortedCollectionName = mergeSortByName(collection);
+// sortedCollectionName.reverse(); // switch to descending
+console.log(sortedCollectionName);
+
 //TEST THE FUNCTION
-void getCardInfo("Sol Ring");
+//void getCardInfo("Sol Ring");
 /*collection.sort((a, b) => a.price - b.price);
 collection.reverse();*/
-for(let i = 0; i < collection.length; i++) {
-    console.log(collection[i]);
-    console.log("\n");
-}
+// for(let i = 0; i < collection.length; i++) {
+//     console.log(collection[i]);
+//     console.log("\n");
+// }
 
 // Search for a card by name
 async function searchSpecificCardName(cardName) {
